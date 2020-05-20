59420982bf37c2a557d4e41b86f715a212c839f1
alexis-karyavin <alexis-karyavin@yandex.ru>
2020-05-21 00:00:52 +0300

    change store file

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,28 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Axios from 'axios';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    data: [],
+  },
+  getters: {
+    DATA(state) {
+      return state.data;
+    }
   },
   mutations: {
+    CHANGE_DATA(state, data) {
+      state.data = data;
+    }
   },
   actions: {
+    async GET_DATA(state, paydata) {
+      const {data} = await Axios.get('https://jsonplaceholder.typicode.com/posts/1/comments');
+      state.commit('CHANGE_DATA', data);
+    }
   },
   modules: {
   }
