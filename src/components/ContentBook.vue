8fc50899dce4b87ab215eef1538a6ebe99b70c9e
alexis-karyavin <alexis-karyavin@yandex.ru>
2020-05-20 23:11:14 +0300

    Create template app

---
+++ src/components/ContentBook.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-content>
+    <v-container fluid>
+      <v-row>
+        <v-col class="ml-4 mr-4" >
+          <router-view></router-view>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-content>
+</template>
+
+<script>
+  export default {
+    name: "ContentBook"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app-bar
+      app
+      color="indigo"
+      dark
+  >
+    <v-app-bar-nav-icon @click.stop="clickBtnMenu"></v-app-bar-nav-icon>
+    <v-toolbar-title>Elecrtonic Book</v-toolbar-title>
+  </v-app-bar>
+</template>
+
+<script>
+  export default {
+    name: "Header",
+    methods: {
+      clickBtnMenu() {
+        this.$emit('toggleSideBar');
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
