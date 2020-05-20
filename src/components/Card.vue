40fff814bfc98874d81cb8028820b97eb3efcbbd
alexis-karyavin <alexis-karyavin@yandex.ru>
2020-05-20 23:32:48 +0300

    Add card component

---
+++ src/components/Card.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-card
+      class=""
+      max-width="300"
+  >
+    <v-img
+        class="white--text align-end"
+        height="200px"
+        :src="image || 'https://cdn.vuetifyjs.com/images/cards/docks.jpg'"
+    >
+      <v-card-title>{{ title || 'Add title'}}</v-card-title>
+    </v-img>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    name: "Card",
+    props: [
+      'title',
+      'image'
+    ]
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="home">
     <h1>Home Page</h1>
+    <Card title="Глава 1. Химические элементы. Атомы и молекулы. Изотопы"/>
   </div>
 </template>
 
 <script>
+  import Card from "../components/Card";
 export default {
   name: 'Home',
+  components: {
+    Card
+  }
 }
 </script>
